0921947d14fca33b2a3ec9b5e3fa4e8dbb845ba7
Thompson Harris <william.thompson.harris@gmail.com>
2019-09-30 21:37:09 -0400

    almost there

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const listObject = [];
 
 document.getElementById('create').addEventListener('click', (ev) => {
     ev.preventDefault();
-    listObject.push({'name' : name.value , 'email' : email.value, 'isVip' : checkbox.checked});
+    listObject.push({'id' : tempCounter ,'name' : name.value , 'email' : email.value, 'isVip' : checkbox.checked});
     const contactItem = `<div class="contactItem"><p id='contact_${tempCounter}'>${listObject[tempCounter].name} (${listObject[tempCounter].email})</p><button id="destroy_${tempCounter}" type="submit">Destroy</button></div>`
     contactList.innerHTML += contactItem;
     
@@ -27,9 +27,16 @@ function destroyLastItem(obj, id){
 
     const button = document.getElementById(`destroy_${id}`);
     button.addEventListener('click', (ev) =>{
+        const tempId = Number(ev.target.id.slice(ev.target.id.lastIndexOf('_') + 1));
+        // delete obj[id];
+        obj.filter(ele=>{
+           if(tempId === ele.id){
+             console.log('delete me')
+           }
+        })
+
+        console.log(obj)
        
-        delete obj[id];
-       console.log(obj)
 
     })
 
